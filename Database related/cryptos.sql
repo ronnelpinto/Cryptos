89006928944954da7a447b7933e56b4e8d4aa169
Ronnel Maclean Pinto <ronnel.pinto@gmail.com>
2014-04-04 01:44:01 +0530

    The database framework
    Steps to open:
    - make a database called cryptos in phpmyadmin
    - 'import' the .sql file and view the same
    
    let me know if any changes required or do the necessary changes

---
+++ Database related/cryptos.sql
@@ -0,0 +1,56 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.2.2
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 03, 2014 at 10:08 PM
+-- Server version: 5.5.27
+-- PHP Version: 5.4.7
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `cryptos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `keytable`
+--
+
+CREATE TABLE IF NOT EXISTS `keytable` (
+  `UserId` int(11) NOT NULL,
+  `AlgoId` int(11) NOT NULL AUTO_INCREMENT,
+  `AlgoName` varchar(30) NOT NULL,
+  `KeyText` text NOT NULL,
+  `Timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`AlgoId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `UserId` int(11) NOT NULL AUTO_INCREMENT,
+  `Username` varchar(30) NOT NULL DEFAULT 'John Doe',
+  `Password` varchar(20) NOT NULL,
+  `First Name` varchar(30) NOT NULL,
+  `Last Name` varchar(30) NOT NULL,
+  `Timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`UserId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
